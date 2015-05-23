9bbe4258ec8cc8faca7a96d150053ecf2be6bec6
Steve Cooper <steve@stevecooper.org>
2015-05-23 19:24:10 +0100

    fist vesion

---
+++ Source/TypeWalker/TypeWalker.Tests/BasicClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeWalker.Tests
+{
+    public class BasicClass
+    {
+        public string GetterSetterString { get; set; }
+        public int GetterPrivateSetterString { get; private set; }
+        private DateTime PrivateGetterSetterDate { get; set; }
+        public bool? NullableGetterSetterBool { get; set; }
+
+        public string StringField;
+
+
+    }
+}
---
+++ Source/TypeWalker/TypeWalker.Tests/VisitorTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TypeWalker.Tests
+{
+    [TestClass]
+    public class VisitorTests
+    {
+        [TestMethod]
+        public void VisitorBasicClass()
+        {
+            var trace = new StringBuilder();
+            var visitor = new Visitor();
+            var typeList = new List<Type> { typeof(BasicClass) };
+
+            visitor.TypeVisiting += (sender, args) => { trace.AppendFormat("interface {0} {{", args.TypeName).AppendLine(); };
+            visitor.TypeVisited +=  (sender, args) => { trace.AppendFormat("}}", args.TypeName).AppendLine(); };
+
+            visitor.MemberVisiting += (sender, args) => { trace.AppendFormat("  {0} : {1};", args.MemberName, args.MemberTypeName).AppendLine(); };
+            //visitor.MemberVisited +=  (sender, args) => { trace.AppendFormat("  }}").AppendLine(); };
+           
+            visitor.Visit(typeList);
+             
+            var actual = trace.ToString().Trim();
+            var expected = @"
+interface BasicClass {
+  GetterSetterString : String;
+  GetterPrivateSetterString : Int32;
+  NullableGetterSetterBool : Nullable<Bool>;
+  StringField : String;
+}
+".Trim();
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
