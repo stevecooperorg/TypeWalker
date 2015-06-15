4cadf86978510367501881ee8b8d1878f95dd8ee
Steve Cooper <steve@stevecooper.org>
2015-06-15 21:56:05 +0100

    steps towards msbuild integration

--- Source/TypeWalker/TypeWalker/pack.bat
+++ Source/TypeWalker/TypeWalker/pack.bat
@@ -1,5 +1,6 @@
 del *.nupkg
 del Build /S/Q
 xcopy NuGet Build\ /E
-xcopy bin\Debug Build\lib\Net40\
+copy bin\Debug\TypeWalker.exe Build\lib\Net40\
+copy bin\Debug\TypeWalker.pdb Build\lib\Net40\
 nuget pack Build/TypeWalker.nuspec
